805f89e8974a8372b2e601458ddc407439453938
Constantine <tmail99@gmail.com>
2025-04-12 14:32:41 +0300

    максимально упрощенный сервер- только список пицц и тест только этого обработчика

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -15,28 +14,68 @@ type Pizza struct {
 
 type Pizzas []Pizza
 
-func (ps Pizzas) FindByID(ID int) (Pizza, error) {
-	for _, pizza := range ps {
-		if pizza.ID == ID {
-			return pizza, nil
-		}
-	}
+// func (ps Pizzas) FindByID(ID int) (Pizza, error) {
+// 	for _, pizza := range ps {
+// 		if pizza.ID == ID {
+// 			return pizza, nil
+// 		}
+// 	}
 
-	return Pizza{}, fmt.Errorf("Couldn't find pizza with ID: %d", ID)
-}
+// 	return Pizza{}, fmt.Errorf("Couldn't find pizza with ID: %d", ID)
+// }
 
-type Order struct {
-	PizzaID  int `json:"pizza_id"`
-	Quantity int `json:"quantity"`
-	Total    int `json:"total"`
-}
+// type Order struct {
+// 	PizzaID  int `json:"pizza_id"`
+// 	Quantity int `json:"quantity"`
+// 	Total    int `json:"total"`
+// }
 
-type Orders []Order
+// type Orders []Order
+
+// type ordersHandler struct {
+// 	pizzas *Pizzas
+// 	orders *Orders
+// }
+
+// func (oh ordersHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+
+// 	switch r.Method {
+// 	case http.MethodPost:
+// 		var o Order
+
+// 		if len(*oh.pizzas) == 0 {
+// 			http.Error(w, "Error: No pizzas found", http.StatusNotFound)
+// 			return
+// 		}
+
+// 		err := json.NewDecoder(r.Body).Decode(&o)
+// 		if err != nil {
+// 			http.Error(w, "Can't decode body", http.StatusBadRequest)
+// 			return
+// 		}
+
+// 		p, err := oh.pizzas.FindByID(o.PizzaID)
+// 		if err != nil {
+// 			http.Error(w, fmt.Sprintf("Error: %s", err), http.StatusBadRequest)
+// 			return
+// 		}
+
+// 		o.Total = p.Price * o.Quantity
+// 		*oh.orders = append(*oh.orders, o)
+// 		json.NewEncoder(w).Encode(o)
+// 	case http.MethodGet:
+// 		json.NewEncoder(w).Encode(oh.orders)
+// 	default:
+// 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+// 	}
+// }
 
 type pizzasHandler struct {
 	pizzas *Pizzas
 }
 
+// здесь только Get - List all pizzas on the menu: GET `/pizzas“
 func (ph pizzasHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -53,47 +92,9 @@ func (ph pizzasHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-type ordersHandler struct {
-	pizzas *Pizzas
-	orders *Orders
-}
-
-func (oh ordersHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	switch r.Method {
-	case http.MethodPost:
-		var o Order
-
-		if len(*oh.pizzas) == 0 {
-			http.Error(w, "Error: No pizzas found", http.StatusNotFound)
-			return
-		}
-
-		err := json.NewDecoder(r.Body).Decode(&o)
-		if err != nil {
-			http.Error(w, "Can't decode body", http.StatusBadRequest)
-			return
-		}
-
-		p, err := oh.pizzas.FindByID(o.PizzaID)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error: %s", err), http.StatusBadRequest)
-			return
-		}
-
-		o.Total = p.Price * o.Quantity
-		*oh.orders = append(*oh.orders, o)
-		json.NewEncoder(w).Encode(o)
-	case http.MethodGet:
-		json.NewEncoder(w).Encode(oh.orders)
-	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
-}
-
 func main() {
-	var orders Orders
+	//var orders Orders
+
 	pizzas := Pizzas{
 		Pizza{
 			ID:    1,
@@ -114,7 +115,7 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/pizzas", pizzasHandler{&pizzas})
-	mux.Handle("/orders", ordersHandler{&pizzas, &orders})
+	//mux.Handle("/orders", ordersHandler{&pizzas, &orders})
 
 	log.Fatal(http.ListenAndServe("localhost:8080", mux))
 }
