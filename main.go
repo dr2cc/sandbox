3d5e56bfe05a021abafd69aa90ed00382354a614
Constantine <tmail99@gmail.com>
2025-09-21 19:16:17 +0300

    *

--- main.go
+++ main.go
@@ -14,49 +14,56 @@ type customer struct {
 }
 
 type creater interface {
+	// Метод create интерфейса creater
 	create(user string, info string)
 }
 
-func (e employer) create(nick string, contact string) {
+func (e *employer) create(nick string, contact string) {
 	e.name = nick
 	e.address = contact
 	e.salary = 99999
-	fmt.Println("Employer", e)
+	fmt.Println("employer", *e)
+	fmt.Println("employer after clean", e.clean())
 }
 
-func (c customer) create(nick string, contact string) {
+func (e employer) clean() employer {
+	e.name = ""
+	e.address = ""
+	e.salary = 0
+	return e
+}
+
+func (c *customer) create(nick string, contact string) {
 	c.name = nick
 	c.email = contact
-	fmt.Println("Customer", c)
+	fmt.Println("customer", *c)
 }
 
-// // Функция для демонстрации работы с интерфейсом
-// func processCreator(c creater, name, info string) {
-// 	c.create(name, info)
-// }
+func processCreator(c creater, name, data string) {
+	c.create(name, data)
+}
 
 func main() {
-	// Создаем экземпляры
-	emp := employer{}
-	cust := customer{}
+	// создаю объекты типов employer и customer
+	empl := &employer{}
+	cust := &customer{}
 
-	// ✅ Оба типа реализуют интерфейс creater
-	var c creater
+	// var _ creater = &employer{}      // employer реализует creater
+	// var _ creater = (*customer)(nil) // customer реализует creater
 
-	// employer реализует creater
-	c = emp
-	c.create("Alice", "Moscow")
+	//var c creater
 
-	// customer реализует creater
-	c = cust
-	c.create("Bob", "bob@email.com")
+	processCreator(empl, "Bob", "5 Avenue")
 
-	// // Использование через функцию
-	// processCreator(emp, "Charlie", "SPb")
-	// processCreator(cust, "David", "david@email.com")
+	processCreator(cust, "Todd", "tt@gg.hhh")
 
-	// // Проверка типа через утверждение типа
-	// if creator, ok := c.(customer); ok {
-	// 	fmt.Println("Это customer:", creator)
-	// }
+	// // employer реализует creater
+	// c = empl
+	// // реализуя такие же методы (поля объекта никак не связаны с интерфейсом)
+	// // другие методы у типа могут быть отличные от интерфейса!
+	// c.create("Bob", "5 Avenue")
+
+	// c = cust
+	// c.create("Todd", "tt@gg.hhh")
+
 }
